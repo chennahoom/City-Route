e6cfc6e1b5ea5a5e1827b3288c06adfc741e8de6
Chen <chen.nahoom@gmail.com>
2021-01-29 12:43:34 +0200

    google auth

--- src/App.js
+++ src/App.js
@@ -1,63 +1,70 @@
-import './App.css';
+import "./App.css";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './Components/Header';
-import TripsPage from './Pages/TripsPage';
-import ResultsPage from './Pages/ResultsPage';
-import HomePage from './Pages/HomePage';
-import MapPage from './Pages/MapPage';
-import Article from './Pages/Article';
-import RegisterPage from './Pages/RegisterPage';
+import Header from "./Components/Header";
+import TripsPage from "./Pages/TripsPage";
+import ResultsPage from "./Pages/ResultsPage";
+import HomePage from "./Pages/HomePage";
+import MapPage from "./Pages/MapPage";
+import Article from "./Pages/Article";
+import RegisterPage from "./Pages/RegisterPage";
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-	const [searhTripForm, setSearhTripForm] = useState({ city: '', start: '', end: '' });
-	const [userTrips, setUserTrips] = useState([]);
-  
-	function updateForm(event) {
-		console.log('updateForm', event.target.name, event.target.value);
+  const [searhTripForm, setSearhTripForm] = useState({
+    city: "",
+    start: "",
+    end: "",
+  });
+  const [userTrips, setUserTrips] = useState([]);
 
-		//const value = event.target.value;
-		//const name = event.target.name;
-		const { value, name } = event.target; // event.target -> DOM ELEMENT THAT FIRE EVENT
+  function updateForm(event) {
+    console.log("updateForm", event.target.name, event.target.value);
 
-		setSearhTripForm({ ...searhTripForm, [name]: value });
-	}
+    //const value = event.target.value;
+    //const name = event.target.name;
+    const { value, name } = event.target; // event.target -> DOM ELEMENT THAT FIRE EVENT
 
-	const updateTrips = (trip) => {
-		setUserTrips([...userTrips, trip]);
-		console.log(userTrips);
-	}
+    setSearhTripForm({ ...searhTripForm, [name]: value });
+  }
 
-	console.log('searhTripForm', searhTripForm);
+  const updateTrips = (trip) => {
+    setUserTrips([...userTrips, trip]);
+    console.log(userTrips);
+  };
 
-	return (
-		<BrowserRouter>
-			<div className="App">
-				<Header />
-				<Switch>
-					<Route path="/trips" exact>
-						<TripsPage updateForm={updateForm} />
-					</Route>
-					<Route path="/results" exact>
-						<ResultsPage searhTripForm={searhTripForm}  updateTrips={updateTrips} />
-					</Route>
-					<Route path="/map/:tripId" exact>
-						<MapPage />
-					</Route>
-					{/* <Route path="/article/:usersId" exact> */}
-					<Route path="/article" exact>
-						<Article userTrips={userTrips} />
-					</Route>
-					<Route path="/register" exact>
-						<RegisterPage />
-					</Route>
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
+  console.log("searhTripForm", searhTripForm);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/trips" exact>
+            <TripsPage updateForm={updateForm} />
+          </Route>
+          <Route path="/results" exact>
+            <ResultsPage
+              searhTripForm={searhTripForm}
+              updateTrips={updateTrips}
+            />
+          </Route>
+          <Route path="/map/:tripId" exact>
+            <MapPage />
+          </Route>
+          {/* <Route path="/article/:usersId" exact> */}
+          <Route path="/article" exact>
+            <Article userTrips={userTrips} />
+          </Route>
+          <Route path="/register" exact>
+            <RegisterPage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -1,19 +1,69 @@
 import { GoogleLogin } from "react-google-login";
+import { useEffect, useState } from "react";
 
-const responseGoogle = (response) => {
-    console.log(response);
-    console.log(response.profileObj);
+import { useGoogleLogout } from 'react-google-login'
 
-  }
+
 function RegisterPage() {
+
+  const { signOut, loaded } = useGoogleLogout({
+    jsSrc,
+    onFailure,
+    clientId,
+    cookiePolicy,
+    loginHint,
+    hostedDomain,
+    fetchBasicProfile,
+    discoveryDocs,
+    uxMode,
+    redirectUri,
+    scope,
+    accessType,
+    onLogoutSuccess
+  })
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    // run after render
+    fetch(`https://city-route.herokuapp.com/api/users`)
+      .then((res) => res.json())
+      .then((body) => {
+        setResults(body);
+      });
+  }, []);
+
+  const responseGoogle = (response) => {
+    for (let i = 0; i < results.length; i++) {
+      if (response.profileObj.email === results[i].email)
+        console.log(results[i].email);
+      else {
+      }
+    }
+
+    // console.log(response);
+    // console.log(response.profileObj);
+
+    // console.log(response.profileObj.email);
+    // console.log(response.profileObj.givenName);
+  };
+
   return (
-    <GoogleLogin
-      clientId="233069535985-vfone0gmelp0cfv62424j18a94av35i3.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={"single_host_origin"}
-    />
+    <div>
+      <GoogleLogin
+        clientId="233069535985-vfone0gmelp0cfv62424j18a94av35i3.apps.googleusercontent.com"
+        buttonText="Login With Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+
+      <GoogleLogout
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      />
+    </div>
   );
 }
 
