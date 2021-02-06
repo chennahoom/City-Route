b49c4c550fca8d33f0a1e43be298413ebdb23a1b
Ariel <ariellunen@gmail.com>
2021-02-06 17:07:44 +0200

    typo

--- src/App.js
+++ src/App.js
@@ -112,143 +112,6 @@ function App() {
 				console.log(user.type_of_user);
 				console.log(user);
   });
-<<<<<<< HEAD
-  const [userTrips, setUserTrips] = useState([]);
-  const [isLogin, setLogin] = useState(false);
-
-  function updateForm(event) {
-    const { value, name } = event.target; // event.target -> DOM ELEMENT THAT FIRE EVENT
-    setSearhTripForm({ ...searhTripForm, [name]: value });
-  }
-
-  // const [results, setResults] = useState([]);
-  const history = useHistory();
-
-  const setlog = (res) => {
-    setLogin(res);
-  };
-
-  const signUp = (newUser) => {
-    if (newUser.type_of_user === "Traveler") {
-      history.push("/trips");
-    } else {
-      history.push("/tourGuideMenu");
-    }
-  };
-
-  const addUser = (newUser) => {
-    fetch(`https://city-route.herokuapp.com/api/users/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(newUser),
-    })
-      .then((response) => response.json())
-      .then((newUser) => {
-        console.log(newUser);
-        setUser(newUser);
-        signUp(newUser);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const addTrip = (newTrip) => {
-    fetch(`https://city-route.herokuapp.com/api/trips/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(newTrip),
-    })
-      .then((response) => response.json())
-      .then((newTrip) => {
-        console.log(newTrip);
-        console.log(newTrip.id);
-        updateUserTrips(newTrip.id);
-        // console.log(newTripId);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const updateUserTrips = (newTripId) => {
-    // setMyTrips();
-    var newMyTrips = user.my_trips;
-    newMyTrips.push(newTripId);
-    fetch(`https://city-route.herokuapp.com/api/users/${user.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        my_trips: newMyTrips,
-      }),
-    })
-      .then((response) => response.json())
-      .then((newTrip) => {
-        console.log(newTrip);
-        console.log(userTrips);
-        // setUserTrips(user.my_trips);
-        // updateTrips(newTrip);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  //Here is the USER!!!
-  console.log("user", user);
-
-  return (
-      <div className="App">
-        <Header setlog={setlog} setUser={setUser} />
-        <Switch>
-          <Route path="/trips" exact>
-            <SearchTripPage updateForm={updateForm} />
-          </Route>
-          <Route path="/register" exact>
-            <Register addUser={addUser} />
-          </Route>
-          <Route path="/results" exact>
-            <ResultsPage
-              searhTripForm={searhTripForm}
-              // updateTrips={updateTrips}
-            />
-          </Route>
-          <Route path="/map/:city" exact>
-            <TripDetailsPage updateUserTrips={updateUserTrips}/>
-          </Route>
-          <Route path="/map/:city" exact>
-            <Map />
-          </Route>
-          {/* <Route path="/article/:usersId" exact> */}
-          <Route path="/MyTripsPage" exact>
-            <MyTripsPage user={user} updateUserTrips={updateUserTrips} />
-          </Route>
-          <Route path="/login" exact>
-            <LoginPage
-              setName={setName}
-              setEmail={setEmail}
-              setUrl={setUrl}
-              name={name}
-              email={email}
-              url={url}
-              setlog={setlog}
-              setUser={setUser}
-              addUser={addUser}
-            />
-          </Route>
-          <Route path="/tourGuideMenu" exact>
-            <TourGuidePage
-              user={user}
-              addTrip={addTrip}
-              updateUserTrips={updateUserTrips}
-              userTrips={userTrips}
-            />
-          </Route>
-          {/* <Route path="/addTrip" exact>
-=======
 		// getAllUsers();
 
 		//findUserByEmail(res.profileObj.email);
@@ -282,7 +145,6 @@ function App() {
 				console.log(newTrip);
 				console.log(newTrip.id);
 				updateUserTrips(newTrip.id);
-				// console.log(newTripId);
 			})
 			.catch(err => console.error(err));
 	};
@@ -361,7 +223,6 @@ function App() {
 					<TourGuidePage user={user} addTrip={addTrip} updateUserTrips={updateUserTrips} userTrips={userTrips} />
 				</Route>
 				{/* <Route path="/addTrip" exact>
->>>>>>> 35c17ec410ecc3842fb492c4f8b0b6d4573d29e3
             <AddTrip />
           </Route> */}
 			</Switch>
--- src/Pages/MyTripsPage.js
+++ src/Pages/MyTripsPage.js
@@ -1,16 +1,6 @@
 import TripResults from '../Components/TripResults';
 
 function MyTripsPage(props) {
-<<<<<<< HEAD
-  return (
-    <div>
-      <h2 id="pageTitle">My Trips</h2>
-      <section className="row row-cols-1 row-cols-md-3" id="All-trips">
-        {props.user.my_trips}
-      </section>
-    </div>
-  );
-=======
 	console.log('USER', props.user);
 	return (
 		<div>
@@ -21,7 +11,6 @@ function MyTripsPage(props) {
 			</section>
 		</div>
 	);
->>>>>>> 35c17ec410ecc3842fb492c4f8b0b6d4573d29e3
 }
 
 export default MyTripsPage;
