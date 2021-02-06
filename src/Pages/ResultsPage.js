f04fd20c975994aabadc247a0efdfbe94415cb32
Chen <chen.nahoom@gmail.com>
2021-02-06 17:09:17 +0200

    github issues

--- src/Pages/ResultsPage.js
+++ src/Pages/ResultsPage.js
@@ -2,46 +2,6 @@ import TripResults from '../Components/TripResults';
 import { useEffect, useState } from 'react';
 
 function ResultsPage(props) {
-<<<<<<< HEAD
-  const [results, setResults] = useState([]);
-
-  useEffect(() => {
-    // run after render
-
-    fetch("https://city-route.herokuapp.com/api/trips")
-      .then((res) => res.json())
-      .then((body) => {
-        setResults(body);
-      });
-  }, []);
-
-  let filteredTrips = results.filter((item) => {
-    const tripDate = stringToDate(item.tour_date);
-    const startDate = stringToDate(props.searhTripForm.start);
-    const endData = stringToDate(props.searhTripForm.end);
-
-    const validDate =
-      tripDate.getTime() <= endData.getTime() &&
-      tripDate.getTime() >= startDate.getTime();
-
-    const validCity = props.searhTripForm.city
-      ? item.trip_name_city === props.searhTripForm.city
-      : true;
-    return validDate && validCity;
-  });
-
-  return (
-    <div>
-      <section className="container">
-        <h2 className="pageTitle">Where would you like to travel?</h2>
-        <section className="row row-cols-1 row-cols-md-3"></section>
-        {filteredTrips.map((trip, i) => (
-          <TripResults trip={trip} key={i}/>
-        ))}
-      </section>
-    </div>
-  );
-=======
 	const [results, setResults] = useState([]);
 
 	useEffect(() => {
@@ -78,7 +38,6 @@ function ResultsPage(props) {
 			</section>
 		</div>
 	);
->>>>>>> 35c17ec410ecc3842fb492c4f8b0b6d4573d29e3
 }
 
 function stringToDate(date) {
