e3585958edbb2a7f0bb3b0830ad0c100d93bd683
Chen <chen.nahoom@gmail.com>
2021-02-06 14:14:25 +0200

    add trips to user

--- src/Pages/MyTripsPage.js
+++ src/Pages/MyTripsPage.js
@@ -1,16 +1,16 @@
-import TripResults from "../Components/TripResults";
+import TripResults from '../Components/TripResults';
 
 function MyTripsPage(props) {
-  console.log();
-  return (
-    <div>
-      <h2 id="pageTitle">My Trips</h2>
+	console.log('USER', props.user);
+	return (
+		<div>
+			<h2 id="pageTitle">My Trips</h2>
 
-      <section className="row row-cols-1 row-cols-md-3" id="All-trips">
-        {props.userTrips}
-      </section>
-    </div>
-  );
+			<section className="row row-cols-1 row-cols-md-3" id="All-trips">
+				{props.userTrips}
+			</section>
+		</div>
+	);
 }
 
 export default MyTripsPage;
--- src/Pages/TripDetailsPage.js
+++ src/Pages/TripDetailsPage.js
@@ -1,11 +1,11 @@
-import TripDetails from "../Components/TripDetails";
+import TripDetails from '../Components/TripDetails';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 function TripDetailsPage(props) {
-  const params = useParams();
-  console.log(params);
-  return <TripDetails tripId={params.tripId} updateTrips={props.updateTrips} />;
+	const params = useParams();
+	console.log(params);
+	return <TripDetails user={props.user} tripId={params.tripId} updateTrips={props.updateTrips} />;
 }
 
 export default TripDetailsPage;
