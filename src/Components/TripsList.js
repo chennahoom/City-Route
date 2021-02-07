93158762afe4dad84f380deeba306c403c4f9bd1
Chen <chen.nahoom@gmail.com>
2021-02-07 21:59:59 +0200

    stash

--- src/Components/TripsList.js
+++ src/Components/TripsList.js
@@ -4,14 +4,6 @@ import React from 'react';
 
 function TripList(props) {
 
-<<<<<<< HEAD
-=======
-    useEffect(() => {  
-      if (props.user.my_trips ==! undefined)
-      setTourGuideTrips(props.user.my_trips);
-      // forceUpdate();
-  }, [props.user.my_trips]);
->>>>>>> 5916770b59089cea42a63ad252600cea9e6dde03
 
 
 
