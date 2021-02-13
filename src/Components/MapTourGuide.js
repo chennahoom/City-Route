0aafd6fdc30898f083d5267fe4537d70551e85ca
Chen <chen.nahoom@gmail.com>
2021-02-13 19:41:38 +0200

    map tourguide

---
+++ src/Components/MapTourGuide.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+
+import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+  Polyline,
+} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+import SearchMap from "./SearchMap";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "750px",
+  height: "500px",
+};
+const options = {
+  //   styles: mapStyles,
+  disableDefaultUI: false,
+  zoomControl: true,
+};
+
+
+
+function Map(props) {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCqp3XhCNtt2GaQgDAhRvrjfO-A8zVQPWc",
+    libraries,
+  });
+
+  const [markers, setMarkers] = useState([]);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
+  if (!props.stops.length) return "loading...";
+
+  return (
+    <div>
+      <SearchMap panTo={panTo} />
+
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={{
+          lat: props.stops[0].location_coords[0]?.lat,
+          lng: props.stops[0].location_coords[0]?.lng,
+        }}
+        options={options}
+        onLoad={onMapLoad}
+      >
+        {props.stops.map((stop, i) => (
+          <Marker
+            key={stop.stop_name}
+            position={{
+              lat: stop.location_coords[0]?.lat,
+              lng: stop.location_coords[0]?.lng,
+            }}
+          />
+        ))}
+      </GoogleMap>
+    </div>
+  );
+}
+
+export default Map;
--- src/Components/TourGuideTrip.js
+++ src/Components/TourGuideTrip.js
@@ -1,47 +1,46 @@
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Map from "../Components/Map";
 
-function TourGuideTrip(props){
-    const [tripData, setTripData] = useState(props.data);
+function TourGuideTrip(props) {
+  const [tripData, setTripData] = useState(props.data);
 
-    useEffect(() => {
-        fetch(`https://city-route.herokuapp.com/api/trips/${props.data}`)
-            .then((res) => res.json())
-            .then((body) => {
-                if(body){
-                    setTripData(body);  
-                }
-            console.log(body);
-            console.log(props.data);
-        });
-    }, []);
-    
-    const handleDelete = () =>{
+  useEffect(() => {
+    fetch(`https://city-route.herokuapp.com/api/trips/${props.data}`)
+      .then((res) => res.json())
+      .then((body) => {
+        if (body) {
+          setTripData(body);
+        }
+        console.log(body);
         console.log(props.data);
-        props.deleteTrip(props.data);
-    }
+      });
+  }, []);
 
-    const stops = tripData.stops;
+  const handleDelete = () => {
+    console.log(props.data);
+    props.deleteTrip(props.data);
+  };
 
-    return(
+
+  return (
+    <div>
+      <div>
         <div>
-            <div>
-                <Map stops = {stops} />
-            </div>
-            <ul>
-                {/* <li>trip id: {tripData.id}</li> */}
-                <li>tickets bought: {tripData?.ticketsBought}</li>
-                <li>start time: {tripData?.start_time}</li>
-                <li>trip name city: {tripData?.trip_name_city}</li>
-                <li>tour date: {tripData?.tour_date}</li>
-                <li>tour time: {tripData?.tour_time}</li>
-                <button onClick={() => props.onDuplicate(tripData)}>Duplicate</button>
-                <button onClick={() => props.setEditTrip(tripData)}>Edit</button>
-                <button onClick={handleDelete}>Delete</button>
-            </ul>
-        </div>
-    );
-
+        </div>{" "}
+      </div>
+      <ul>
+        {/* <li>trip id: {tripData.id}</li> */}
+        <li>tickets bought: {tripData?.ticketsBought}</li>
+        <li>start time: {tripData?.start_time}</li>
+        <li>trip name city: {tripData?.trip_name_city}</li>
+        <li>tour date: {tripData?.tour_date}</li>
+        <li>tour time: {tripData?.tour_time}</li>
+        <button onClick={() => props.onDuplicate(tripData)}>Duplicate</button>
+        <button onClick={() => props.setEditTrip(tripData)}>Edit</button>
+        <button onClick={handleDelete}>Delete</button>
+      </ul>
+    </div>
+  );
 }
 
 export default TourGuideTrip;
