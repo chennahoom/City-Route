ce6827c5808ac29423b6e349038d8e22f2e1c913
halel-sarusi <halels1401@gmail.com>
2021-02-23 10:54:48 +0200

    my-trip

--- src/Pages/MyTripsPage.js
+++ src/Pages/MyTripsPage.js
@@ -3,11 +3,13 @@ import MyTrips from '../Components/MyTrips';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
+
 const useStyles = makeStyles({
 	gridContainer: {
 		paddingLeft: "15px",
 		paddingRight: "15px",
-	}
+		marginTop: "100px",
+	},
 });
 
 function MyTripsPage(props) {
@@ -15,7 +17,6 @@ function MyTripsPage(props) {
 	const classes = useStyles();
 	return (
 		<Grid container justify="center" className={classes.gridContainer}>
-			<h2 id="pageTitle">My Trips</h2>
 			{props.user?.my_trips.map((tripId, i) =>(
 				<MyTrips key={i} data={tripId} />
 			))}
